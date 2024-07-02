5095513a93df8e8c39883d7cde339a26a75d623b
Luis De La Hoz <luisgdelahozl@gmail.com>
2024-07-02 17:13:46 -0400

    :construction: Create types folder

---
+++ src/lib/types/index.ts
@@ -0,0 +1,21 @@
+import type {
+  InstanceWithExtensions,
+  MagicSDKAdditionalConfiguration,
+  MagicSDKExtensionsOption,
+  SDKBase,
+} from '@magic-sdk/provider'
+import { OAuthRedirectResult } from '@magic-ext/oauth'
+import { DeepLinkPage, MagicUserMetadata } from 'magic-sdk'
+import type { OAuthExtension, OAuthProvider } from '@magic-ext/oauth'
+
+export type {
+  DeepLinkPage,
+  InstanceWithExtensions,
+  MagicSDKAdditionalConfiguration,
+  MagicSDKExtensionsOption,
+  MagicUserMetadata,
+  OAuthExtension,
+  OAuthProvider,
+  OAuthRedirectResult,
+  SDKBase,
+}
