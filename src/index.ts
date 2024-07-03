ab798d1af86dcd98d7f2e55dc3f223ab19526dc3
Luis De La Hoz <luisgdelahozl@gmail.com>
2024-07-03 14:48:59 -0400

    :construction: Export constants, types and magic connector from root index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './lib/connectors/dedicatedWalletConnector'
 export * from './lib/connectors/universalWalletConnector'
+export * from './lib/connectors/magicConnector'
+export * from './lib/constants'
+export * from './lib/types'
